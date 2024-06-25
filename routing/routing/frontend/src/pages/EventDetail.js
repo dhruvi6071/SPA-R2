ed9d05d6aa12638d83e2f11b099b501f169b06a9
dhruvi6071 <dhruvi6071@gmail.com>
2024-06-26 01:27:45 +0530

    task-07-app.js-printid-using-params

--- routing/routing/frontend/src/pages/EventDetail.js
+++ routing/routing/frontend/src/pages/EventDetail.js
@@ -1,7 +1,11 @@
+import { useParams} from 'react-router-dom';
+
 function EventDetail() {
+    const params = useParams();
     return (
         <>
             <h1>Event Detail</h1>
+            <p>Event id : {params.eventId}</p>
         </>
     )
 }
