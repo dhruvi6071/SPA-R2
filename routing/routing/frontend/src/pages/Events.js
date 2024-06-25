cae1caf4bc6acc0aa365243a90f614cfbdba4300
dhruvi6071 <dhruvi6071@gmail.com>
2024-06-26 01:23:35 +0530

    task-6-dummy-data

--- routing/routing/frontend/src/pages/Events.js
+++ routing/routing/frontend/src/pages/Events.js
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom";
+const DUMMY_DATA = [
+  {
+    id: "e1",
+    title: "Some event",
+  },
+  {
+    id: "e2",
+    title: "Another event",
+  },
+];
+
 function Events() {
-    return (
-        <>
-            <h1>Events</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Events</h1>
+      <ul>
+        {DUMMY_DATA.map(event => <li key={event.id}>
+            <Link to={event.id}>{event.title}</Link>
+        </li>)}
+      </ul>
+    </>
+  );
 }
 
 export default Events;
