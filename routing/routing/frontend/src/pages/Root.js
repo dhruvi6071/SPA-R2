ea186329a8877a4b2113b335eae2047845636755
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-01 23:52:47 +0530

    more-use-of-useLoad

--- routing/routing/frontend/src/pages/Root.js
+++ routing/routing/frontend/src/pages/Root.js
@@ -1,8 +1,10 @@
 import React from "react";
 import MainNavigation from '../components/MainNavigation';
-import { Outlet} from 'react-router-dom';
+import { Outlet, useLoaderData} from 'react-router-dom';
 function RootLayout() {
+    const event = useLoaderData();
     return (
+        
         <>
         <MainNavigation />
             <main>
