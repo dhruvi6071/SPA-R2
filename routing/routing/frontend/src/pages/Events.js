cd23d8dca5d5f928ee6c5616562043bda688449a
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-04 15:48:42 +0530

    error-manually

--- routing/routing/frontend/src/pages/Events.js
+++ routing/routing/frontend/src/pages/Events.js
@@ -4,6 +4,9 @@ import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
+  if(data.isError){
+    return <p>{data.message}</p>
+  }
   const events = data.events;
   return <EventsList events={events} />;
 }
@@ -11,11 +14,13 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
+
   // We can't use react hooks into loaders, because loader is not a react component. But we can use all js components.
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    //incorrect response set.
+    return {isError: true, message: 'Could not fetch events'};
+
   } else {
     return response;
   }
