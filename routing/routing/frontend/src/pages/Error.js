02020dcde410e691d337385138403cfa80c90620
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-09 00:08:06 +0530

    expressing-error-data-and-throwing-responses

--- routing/routing/frontend/src/pages/Error.js
+++ routing/routing/frontend/src/pages/Error.js
@@ -1,20 +1,28 @@
 import PageContent from "../components/PageContent";
 import { useRouteError } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
 function ErrorPage() {
-    const error = useRouteError();
-    // eslint-disable-next-line no-unused-expressions
-    // error.status;
+  const error = useRouteError();
+  // eslint-disable-next-line no-unused-expressions
+  // error.status;
 
-    let title = 'An error occured';
-    let message = "something may be wrong";
-    if(error.status === 500){
-        message = error.data.message;
-    }
-    return <PageContent title="An error occured" >
-        
-            <p>Something went wrong</p>
-    
-        </PageContent>;
+  let title = "An error occured";
+  let message = "something may be wrong";
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+  if (error.status === 404) {
+    title = "Not found";
+    message = "could not find resource or page";
+  }
+  return (
+    <>
+    <MainNavigation/> 
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
 }
 
 export default ErrorPage;
