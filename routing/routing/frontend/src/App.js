75f9ff51a1dcc37e4b5631254a63d9a801938979
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-26 23:37:04 +0530

    counting()-usage

--- routing/routing/frontend/src/App.js
+++ routing/routing/frontend/src/App.js
@@ -8,7 +8,7 @@ import EventDetailPage, {
 import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventsRootLayout from './pages/EventsRoot';
 import HomePage from './pages/HomePage';
-import NewEventPage from './pages/NewEventPage';
+import NewEventPage, {action as newEventAction} from './pages/NewEventPage';
 import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
@@ -41,7 +41,7 @@ const router = createBrowserRouter([
             ]
           
           },
-          { path: 'new', element: <NewEventPage /> },
+          { path: 'new', element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
