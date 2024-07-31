f6013ad6c406e4a77030fd9c0c15213c835f8699
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-31 14:08:12 +0530

    updating-ui-state-of-save-button-based-on-submission state

--- routing/routing/frontend/src/pages/NewEventPage.js
+++ routing/routing/frontend/src/pages/NewEventPage.js
@@ -2,7 +2,7 @@ import { json, redirect } from "react-router-dom";
 import EventForm from "../components/EventForm";
 
 function NewEventPage() {
-     return <EventForm></EventForm>
+     return <EventForm></EventForm>;
 }
 
 export default NewEventPage;
@@ -15,7 +15,7 @@ export async function action({request, params}) {
         date: data.get('date'),
         description: data.get('description'),
     }  
-     const response = await fetch('http://localhost:8080/', {
+     const response = await fetch('http://localhost:8080/events', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -27,5 +27,5 @@ export async function action({request, params}) {
         throw json({message: 'Could not send the data'}, {status: 500});
     }
     
-    return redirect('/');
+    return redirect('/events');
 }
