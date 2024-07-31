52f9f38e48874da5cb724ecedf907d5226529722
dhruvi6071 <dhruvi6071@gmail.com>
2024-07-31 15:42:15 +0530

    usefetcher-hook-sign-up-event

--- routing/routing/frontend/src/components/NewsLetterSignUp.js
+++ routing/routing/frontend/src/components/NewsLetterSignUp.js
@@ -1,15 +1,26 @@
+import { useEffect } from 'react';
 import classes from './NewsLetterSignUp.module.css';
-
+import {useFetcher } from 'react-router-dom';
 function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state} = fetcher;
+
+  useEffect(() => {
+    if(state === 'idle' && data && data.message) {
+      window.alert('Signup successfull');
+    };
+  }, [data, state]);
+
   return (
-    <form method="post" className={classes.newsletter}>
+    //Here fetcher.form will initialize an form but does not affect the route.
+    <fetcher.Form method="post" className={classes.newsletter} action="/newsletter">
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
